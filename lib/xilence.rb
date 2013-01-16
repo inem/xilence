61d9ce8c83fcc6088244f8dc926b99beaec99bdd
Maxim Kaschenko <mkaschenko@me.com>
2013-01-16 19:15:20 +0700

    Redefined ActionController::Rescue#rescue_with_handler

--- lib/xilence.rb
+++ lib/xilence.rb
@@ -1,5 +1,24 @@
 require "xilence/version"
 
-module Xilence
+module ActionController
+  module Rescue
 
+    def rescue_with_handler(exception)
+      if request.xhr?
+        linebreaker = "\n  "
+        title = "#{exception.class} (#{exception.message}):#{linebreaker}"
+        backtrace = exception.backtrace.join(linebreaker)
+
+        render text: (title + backtrace), status: 500
+      else
+        if (exception.respond_to?(:original_exception) &&
+            (orig_exception = exception.original_exception) &&
+            handler_for_rescue(orig_exception))
+          exception = orig_exception
+        end
+        super(exception)
+      end
+    end
+
+  end
 end
