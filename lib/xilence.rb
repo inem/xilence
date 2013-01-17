c4cec7714ecc0aca320cef7b548f7b5ba6ae5194
Maxim Kaschenko <mkaschenko@me.com>
2013-01-18 00:46:51 +0700

    The stolen backtrace is found

--- lib/xilence.rb
+++ lib/xilence.rb
@@ -1,22 +1,40 @@
 require "xilence/version"
 
-module ActionController
-  module Rescue
+module ActionDispatch
+  class DebugExceptions
 
-    def rescue_with_handler(exception)
-      if request.xhr?
-        linebreaker = "\n  "
-        title = "#{exception.class} (#{exception.message}):#{linebreaker}"
-        backtrace = exception.backtrace.join(linebreaker)
+    def render_exception(env, exception)
+      wrapper = ExceptionWrapper.new(env, exception)
+      log_error(env, wrapper)
 
-        render text: (title + backtrace), status: 500
-      else
-        if (exception.respond_to?(:original_exception) &&
-            (orig_exception = exception.original_exception) &&
-            handler_for_rescue(orig_exception))
-          exception = orig_exception
+      if env['action_dispatch.show_detailed_exceptions']
+        request = Request.new(env)
+
+        if request.xhr?
+          linebreaker = "\n  "
+
+          title = "#{wrapper.exception.class} (#{wrapper.exception.message}):#{linebreaker}"
+
+          trace = wrapper.application_trace
+          trace = wrapper.framework_trace if trace.empty?
+          trace = trace.join linebreaker
+
+          render(500, title + trace)
+        else
+          template = ActionView::Base.new([RESCUES_TEMPLATE_PATH],
+            :request => request,
+            :exception => wrapper.exception,
+            :application_trace => wrapper.application_trace,
+            :framework_trace => wrapper.framework_trace,
+            :full_trace => wrapper.full_trace
+          )
+
+          file = "rescues/#{wrapper.rescue_template}"
+          body = template.render(:template => file, :layout => 'rescues/layout')
+          render(wrapper.status_code, body)
         end
-        super(exception)
+      else
+        raise exception
       end
     end
 
